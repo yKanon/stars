81c7964613103aecc3fcea85082fdeed05d466f8
ex-ea <394769450@qq.com>
2020-06-16 12:13:29 +0800

    add css precompiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 import Button, { ButtonSize, ButtonType } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Button>这是一个按钮</Button>
+        <Button disabled>这是一个按钮</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
           这是一个按钮
         </Button>
         <Button btnType={ButtonType.Link} href="http://www.baidu.com">
-          这是一个按钮
+          百度Link
         </Button>
       </header>
+
+      <code>hello world</code>
+      <a href="http://www.baidu.com">百度</a>
     </div>
   )
 }
