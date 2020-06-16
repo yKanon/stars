55ab48828cdaa98766db89f549c9571645d66a6c
ex-ea <394769450@qq.com>
2020-06-16 11:19:48 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import Button from './components/Button/button'
+import Button, { ButtonSize, ButtonType } from './components/Button/button'
 
 function App() {
   return (
@@ -10,6 +10,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button>这是一个按钮</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          这是一个按钮
+        </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+          这是一个按钮
+        </Button>
       </header>
     </div>
   )
