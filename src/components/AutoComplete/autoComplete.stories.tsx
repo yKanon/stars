41ec853db6a56e8d2d79189cb77d386a1d15a669
ex-ea <394769450@qq.com>
2020-07-30 21:54:20 +0800

    comments

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -14,36 +14,36 @@ interface GithubUserProps {
   avatar_url: string;
 }
 
-const owWithNumber = [
-  { value: "美", number: 11 },
-  { value: "麦爹", number: 1 },
-  { value: "法鸡", number: 4 },
-  { value: "安娜", number: 2 },
-  { value: "巴蒂", number: 15 },
-  { value: "毛妹", number: 23 },
-  { value: "老鼠", number: 3 },
-  { value: "源氏", number: 14 },
-  { value: "死神", number: 39 },
-  { value: "大锤", number: 12 },
-  { value: "西格玛", number: 30 },
-  { value: "天使姐姐", number: 42 },
-];
+// const owWithNumber = [
+//   { value: "美", number: 11 },
+//   { value: "麦爹", number: 1 },
+//   { value: "法鸡", number: 4 },
+//   { value: "安娜", number: 2 },
+//   { value: "巴蒂", number: 15 },
+//   { value: "毛妹", number: 23 },
+//   { value: "老鼠", number: 3 },
+//   { value: "源氏", number: 14 },
+//   { value: "死神", number: 39 },
+//   { value: "大锤", number: 12 },
+//   { value: "西格玛", number: 30 },
+//   { value: "天使姐姐", number: 42 },
+// ];
 
 const SimpleComplete = () => {
-  const ow = [
-    "美",
-    "麦爹",
-    "法鸡",
-    "安娜",
-    "巴蒂",
-    "毛妹",
-    "老鼠",
-    "源氏",
-    "死神",
-    "大锤",
-    "西格玛",
-    "天使姐姐",
-  ];
+  // const ow = [
+  //   "美",
+  //   "麦爹",
+  //   "法鸡",
+  //   "安娜",
+  //   "巴蒂",
+  //   "毛妹",
+  //   "老鼠",
+  //   "源氏",
+  //   "死神",
+  //   "大锤",
+  //   "西格玛",
+  //   "天使姐姐",
+  // ];
 
   // const fetchSuggestion = (query: string) => {
   //   return ow
